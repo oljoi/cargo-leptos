bb2cbb070e61a115fcce3747dac5eb3efe625c82
oljoi <46825856+oljoi@users.noreply.github.com>
2025-12-25 23:27:57 +0300

    updated flake.nix and added packaging via crane

--- flake.nix
+++ flake.nix
@@ -1,25 +1,68 @@
-
 {
-  description = "A basic Rust devshell";
-
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system overlays;
+          inherit system;
+          overlays = [ (import rust-overlay) ];
         };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain (p: p.rust-bin.nightly.latest.default);
+
+        crateExpr =
+          {
+            perl,
+            openssl,
+            libiconv,
+            lib,
+            pkg-config,
+            stdenv,
+          }:
+          craneLib.buildPackage {
+            src = craneLib.cleanCargoSource ./.;
+            strictDeps = true;
+
+            doCheck = false;
+
+            nativeBuildInputs = [
+              perl
+              pkg-config
+            ]
+            ++ lib.optionals stdenv.buildPlatform.isDarwin [
+              libiconv
+            ];
+
+            buildInputs = [
+              openssl
+            ];
+          };
+
+        cargo-leptos = pkgs.callPackage crateExpr { };
       in
-      with pkgs;
       {
-        devShells.default = mkShell {
-          buildInputs = [
+        packages.default = cargo-leptos;
+
+        devShells.default = craneLib.devShell {
+          packages = with pkgs; [
             openssl
             pkg-config
             cargo-insta
@@ -27,17 +70,7 @@
             llvmPackages_latest.bintools
             zlib.out
             llvmPackages_latest.lld
-            (rust-bin.stable.latest.default.override {
-              extensions= [ "rust-src" "rust-analyzer" ];
-              targets = [ "wasm32-unknown-unknown" ];
-            })
           ];
-
-          shellHook = ''
-            alias ls=exa
-            alias find=fd
-            alias grep=ripgrep
-            '';
         };
       }
     );
