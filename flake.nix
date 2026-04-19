2452e44bb960cb3cdbd8589066e7c4ad5f671515
oljoi <oljoi@duck.com>
2026-04-19 19:42:32 +0300

    nix: removed flake-utils, rewrote flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     crane.url = "github:ipetkov/crane";
-    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,77 +13,97 @@
       self,
       nixpkgs,
       crane,
-      flake-utils,
       rust-overlay,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+
+      mkPkgs =
+        system:
+        import nixpkgs {
           inherit system;
           overlays = [ (import rust-overlay) ];
         };
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = mkPkgs system;
+          craneLib = (crane.mkLib pkgs).overrideToolchain (p: p.rust-bin.nightly.latest.default);
 
-        craneLib = (crane.mkLib pkgs).overrideToolchain (p: p.rust-bin.nightly.latest.default);
+          crateExpr =
+            {
+              perl,
+              openssl,
+              libiconv,
+              lib,
+              pkg-config,
+              stdenv,
+            }:
+            craneLib.buildPackage {
+              src = craneLib.cleanCargoSource ./.;
+              strictDeps = true;
+              doCheck = false;
+              nativeBuildInputs = [
+                perl
+                pkg-config
+              ]
+              ++ lib.optionals stdenv.buildPlatform.isDarwin [
+                libiconv
+              ];
+              buildInputs = [
+                openssl
+              ];
+            };
+        in
+        {
+          default = pkgs.callPackage crateExpr { };
+        }
+      );
 
-        crateExpr =
-          {
-            perl,
-            openssl,
-            libiconv,
-            lib,
-            pkg-config,
-            stdenv,
-          }:
-          craneLib.buildPackage {
-            src = craneLib.cleanCargoSource ./.;
-            strictDeps = true;
-
-            doCheck = false;
-
-            nativeBuildInputs = [
-              perl
-              pkg-config
-            ]
-            ++ lib.optionals stdenv.buildPlatform.isDarwin [
-              libiconv
-            ];
-
-            buildInputs = [
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = mkPkgs system;
+          craneLib = (crane.mkLib pkgs).overrideToolchain (p: p.rust-bin.nightly.latest.default);
+        in
+        {
+          default = craneLib.devShell {
+            packages = with pkgs; [
               openssl
+              pkg-config
+              cargo-insta
+              llvmPackages_latest.llvm
+              llvmPackages_latest.bintools
+              zlib.out
+              llvmPackages_latest.lld
+              (rust-bin.nightly.latest.default.override {
+                extensions = [
+                  "rust-src"
+                  "rust-analyzer"
+                ];
+                targets = [ "wasm32-unknown-unknown" ];
+              })
+              eza
+              fd
+              ripgrep
             ];
-          };
 
-        cargo-leptos = pkgs.callPackage crateExpr { };
-      in
-      {
-        packages.default = cargo-leptos;
-
-        devShells.default = craneLib.devShell {
-          packages = with pkgs; [
-            openssl
-            pkg-config
-            cargo-insta
-            llvmPackages_latest.llvm
-            llvmPackages_latest.bintools
-            zlib.out
-            llvmPackages_latest.lld
-            (rust-bin.stable.latest.default.override {
-              extensions= [ "rust-src" "rust-analyzer" ];
-              targets = [ "wasm32-unknown-unknown" ];
-            })
-	    eza
-	    fd
-	    ripgrep
-          ];
-
-          shellHook = ''
-            alias ls=exa
-            alias find=fd
-            alias grep=ripgrep
+            shellHook = ''
+              alias ls=eza
+              alias find=fd
+              alias grep=ripgrep
             '';
-        };
-      }
-    );
+          };
+        }
+      );
+    };
 }
